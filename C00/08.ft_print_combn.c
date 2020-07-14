e4350dbb52b4e4891f4ba3b7c027785ac9e7f19b
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-14 11:21:14 +0200

    Update 08.ft_print_combn.c

--- C00/08.ft_print_combn.c
+++ C00/08.ft_print_combn.c
@@ -1,15 +1,17 @@
 #include <unistd.h>
 
-void	ft_putchar(char c){
+void	ft_putchar(char c)
+{
 	write(1, &c, 1);
 }
 
-int		power(int n, int exp)
+int	power(int n, int exp)
 {
 	int result = n;
-	while(exp > 1){
-		result *= n;
-		--exp;
+	while(exp > 1)
+	{
+	    result *= n;
+	    --exp;
 	}
 	return result;
 }
@@ -18,23 +20,26 @@ void	ft_printnbr(int nb, int n)
 {
 	if (nb < 10)
 	{
-		ft_putchar(nb + '0');
+	    ft_putchar(nb + '0');
 	}
 	else if (nb >= 10)
 	{
-		ft_printnbr(nb / 10, n);
-		ft_putchar(nb % 10 + '0');
+	    ft_printnbr(nb / 10, n);
+	    ft_putchar(nb % 10 + '0');
 	}
 }
 
-void	less(int n, int total){
-	while(n*10 < total){
-		ft_putchar('0');
-		n *= 10;
+void	less(int n, int total)
+{
+	while(n*10 < total)
+	{
+	    ft_putchar('0');
+	    n *= 10;
 	}
 }
 
-int		is_last_one(int n, int exp){
+int	is_last_one(int n, int exp)
+{
 	if(exp == 1)
 		return n == 9;
 	else if(exp == 2)
@@ -56,21 +61,26 @@ int		is_last_one(int n, int exp){
 	else return 1;
 }
 
-void	ft_print_combn(int n){
+void	ft_print_combn(int n)
+{
 	int i = 1;
 	int x;
 	int total = power(10, n);
 
-	while(i < total){
+	while(i < total)
+	{
 		int j = i;
 		int b = 1;
-		while(j >= 10){
-			if(j % 10 <= (j/10)%10){
+		while(j >= 10)
+		{
+			if(j % 10 <= (j/10)%10)
+			{
 				b = 0;
 			}
 			j /= 10;
 		}
-		if(b == 1) {
+		if(b == 1)
+		{
 			less(i, total);
 			ft_printnbr(i, n);
 			if(!is_last_one(i, n)) ft_putchar(' ');
@@ -79,6 +89,7 @@ void	ft_print_combn(int n){
 	}
 }
 
-int main(){
+int 	main(void)
+{
 	ft_print_combn(2);
 }
