c5a2416a928b74df64907cea4d2e6c319b5239b2
Balbader <15971200+Balbader@users.noreply.github.com>
2020-07-14 11:17:46 +0200

    Update 06.ft_print_comb2.c

--- C00/06.ft_print_comb2.c
+++ C00/06.ft_print_comb2.c
@@ -10,7 +10,6 @@ void    ft_print_comb2(void)
     int    b;
 
     a = 0;
-
     while (a <= 98)
     {
         b = a + 1;
@@ -37,30 +36,3 @@ int     main(void)
      ft_print_comb2();
      return (0);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
