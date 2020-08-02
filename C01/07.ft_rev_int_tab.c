befa69a0503d6e018339f4c65671c070b51aa44e
Balbader <15971200+Balbader@users.noreply.github.com>
2020-08-02 19:37:43 +0200

    Update 07.ft_rev_int_tab.c

--- C01/07.ft_rev_int_tab.c
+++ C01/07.ft_rev_int_tab.c
@@ -1,40 +1,24 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void    ft_swap(int *a, int *b)
-{
-    int     x;
-    x = *a;
-    *a = *b;
-    *b = x;
-}
 void    ft_rev_int_tab(int *tab, int size)
 {
-    int     i;
-    int     j;    
-
-    i = 0;
-    j = size - 1;
-
-    while (i < j)
+    int     *end_tab = (tab + size - 1);
+    
+    while (tab <= end_tab)
     {
-        ft_swap(&tab[i], &tab[j]);
-        i++;
-        j--;
+        printf("%d ", *end_tab);
+        end_tab--;
     }
 }
-/* Need to understand how to print the result to the screen
- *
+
 int     main(void)
 {
+    int     arr[] = {1, 2, 3, 4, 5};
     int     size;
-    int     *tab;
-    
-    size = 9;
-    *tab = size;
 
-    ft_rev_int_tab(tab, size);
-    printf("%d ", size);
+    size = 5;
+
+    ft_rev_int_tab(arr, size);
     return (0);
 }
-*/ 
