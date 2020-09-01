90fefab268a8c794aa91e39b00f7e349adc652d1
Balbader <15971200+Balbader@users.noreply.github.com>
2020-09-01 11:48:21 +0200

    Update 03.ft_atoi.c

--- C04/03.ft_atoi.c
+++ C04/03.ft_atoi.c
@@ -3,13 +3,13 @@
 
 int     ft_atoi(char *str)
 {
+    int     i; 
     int     res;
-    int     negative;
-    int     i;
+	int     sign;
 
-    negative = 0;
-    res = 0;
-    i = 0;
+    i = 0; // Initialize index of first digit
+    res = 0; // Initialize result
+    sign = 0; // Initialize sign as negative
     
     /* if str starts with an arbitrary amount of white-space chatacter */
 	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
@@ -17,16 +17,18 @@ int     ft_atoi(char *str)
     
     /* if str is followed by an arbitrary amount of + and - signs */
 	while (str[i] == '-' || str[i] == '+')
+		
         /* change int sign based on the number of - and if that number is even or odd */
 		if (str[i++] == '-')
-			negative = 1 - negative;
+			sign = 1 - sign;
+	
 	/* if str is followed by number of the base 10 */
 	while (str[i] >= '0' && str[i] <= '9')
     	{
-		res = res * 10 + str[i] - 48;
-        	str++;
+		res = res * 10 + str[i] - '0';
+        	i++;
     	}
-    	return (negative * (res * -1));
+   	return (sign * (res * -1));
 }
 
 int     main(void)
